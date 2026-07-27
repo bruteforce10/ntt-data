7f36daf19bd0a1d09feca1ca6fb6d3f0b2aeacdc
bruteforce10 <audifirdi@gmail.com>
2026-07-27 17:18:50 +0700

    chore(page): comment out Countdown component in Home

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       <Benefit />
       <ProblemOverview />
       <Roadmap />
-      <Countdown />
+      {/* <Countdown /> */}
       <Faq />
       <Footer />
     </main>
